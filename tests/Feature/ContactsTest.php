115b7a2aebb87fc890d69ae0711515605eb4ff80
Cortess <CortessHack@gmail.com>
2019-09-08 19:46:42 +0300

    Add validation tests

--- tests/Feature/ContactsTest.php
+++ tests/Feature/ContactsTest.php
@@ -15,8 +15,50 @@ class ContactsTest extends TestCase
     public function a_contact_can_be_added()
     {
         $this->withoutExceptionHandling();
-        $this->post('/api/contacts',['name' => 'Test name']);
+        $this->post('/api/contacts',[
+            'name' => 'Test name',
+            'email' => 'test@test.com',
+            'birthday' => '09/03/1998',
+            'company' => 'ABC string',
+        ]);
 
-        $this->assertCount(1,Contact::all());
+        $contact = Contact::first();
+
+        $this->assertEquals('Test name', $contact->name);
+        $this->assertEquals('test@test.com', $contact->email);
+        $this->assertEquals('09/03/1998', $contact->birthday);
+        $this->assertEquals('ABC string', $contact->company);
+    }
+
+
+    /** @test */
+    public function a_name_is_required()
+    {
+        $response = $this->post('/api/contacts', [
+            'email' => 'test@test.com',
+            'birthday' => '09/03/1998',
+            'company' => 'ABC string',
+        ]);
+
+
+        $response->assertSessionHasErrors('name');
+        $this->assertCount(0,Contact::all());
+
+    }
+
+
+    /** @test */
+    public function a_email_is_required()
+    {
+        $response = $this->post('/api/contacts', [
+            'name' => 'Test name',
+            'birthday' => '09/03/1998',
+            'company' => 'ABC string',
+        ]);
+
+
+        $response->assertSessionHasErrors('email');
+        $this->assertCount(0,Contact::all());
+
     }
 }
